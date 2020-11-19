fe9426f5662fada7c8cacba09464272d7095ec08
B. Ricks <bricks@unomaha.edu>
2020-11-19 00:53:39 -0600

    Write to image file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import flatGenerator from "./src/flatGenerator.js"
-
+import fs from "fs";
 console.log(flatGenerator(10, 20))
+fs.writeFileSync("./index.obj", flatGenerator(10, 20));
 
 export {flatGenerator};
--- src/flatGenerator.js
+++ src/flatGenerator.js
@@ -1,10 +1,12 @@
 import is from "is_js";
+import PNG from 'pngjs';
+import fs from 'fs';
 /**
  * Remove leading and trailing white space on every line of the obj file. 
  * @param {string-like} objString The obj string to be cleaned.
  */
-function clean(objString){
-  return objString.trim().split("\n").map(s=>s.trim()).join("\n");
+function clean(objString) {
+  return objString.trim().split("/\r?\n/").map(s => s.trim()).join("\n");
 }
 
 /**
@@ -12,18 +14,49 @@ function clean(objString){
  * @param {number-like} width The width of the obj
  * @param {number-like} height The height of the obj
  */
-function flatGenerator(width, height){
-  if(!is.all.finite([width,height]) || arguments.length != 2) throw new "Invalid arguments."
- let toReturn =  
- `
- v ${width/2} 0 -${height/2}
- v ${width/2} 0 ${height/2}
- v -${width/2} 0 -${height/2}
- v -${width/2} 0 ${height/2}
+function flatGenerator(width, height, generateTexture) {
+  if (!is.all.finite([width, height]) || arguments.length != 2) throw new "Invalid arguments."
+  let toReturn =
+    `  
+ v -${width / 2} 0 -${height / 2}
+ v -${width / 2} 0 ${height / 2}
+ v ${width / 2} 0 ${height / 2}
+ v ${width / 2} 0 -${height / 2}
+ vt 0 0
+ vt 0 1
+ vt 1 1 
+ vt 1 0
  vn 0 1 0
- f 1//1 3//1 4//1 2//1
+ f 1/1/1 2/2/1 3/3/1 4/4/1
  `;
- return clean(toReturn);
+
+
+// https://stackoverflow.com/a/32260288/10047920
+  var png = new PNG.PNG({
+    width: 100,
+    height: 100,
+    filterType: -1
+  });
+
+  for (var y = 0; y < png.height; y++) {
+    for (var x = 0; x < png.width; x++) {
+      var idx = (png.width * y + x) << 2;
+      png.data[idx] = 255;
+      png.data[idx + 1] = 218;
+      png.data[idx + 2] = 185;
+      png.data[idx + 3] = 128;
+    }
+  }
+
+  png.pack().pipe(fs.createWriteStream('newOut.png'));
+
+
+
+  if (generateTexture) {
+
+  }
+  return clean(toReturn);
+  //return toReturn;
 }
 
 export default flatGenerator;
