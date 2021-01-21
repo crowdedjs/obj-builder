c543867be28f77066bcd87da8b2dfedf35c916cc
waramsey <waramsey@unomaha.edu>
2021-01-20 22:59:28 -0700

    Commits before working on rotation

---
+++ src/YLayout.js
@@ -0,0 +1,83 @@
+import fs from "fs";
+import { allocate, basicFill, lineFill, generateLabels, visualizeEmpty, checkForHalls, threeHallFill, fourHallFill } from "./spacesSharedFunctions.js"
+import { makeRoom } from "./room.js";
+import flatGenerator from "./flatGenerator.js";
+import { makeWalls } from "./outerWalls.js";
+
+const w = Math.floor(Math.random() * 150) + 51;
+const l = Math.floor(Math.random() * 150) + 51;
+const defaultSpace = {
+    TL:{x:-w/2,y:-l/2},
+    BR:{x:w/2,y:l/2}
+}
+
+export function YLayout(filePath = "test", spaceToFill = defaultSpace, hallWidth = 6) {
+    fs.writeFileSync(filePath + `.obj`, "mtllib room.mtl\n");
+    fs.writeFileSync(filePath + `.mtl`, "\n");
+
+    let emptySpace = [];
+    let filledSpace = [];
+    
+    let vOffset = 0;
+    emptySpace.push(spaceToFill);
+    
+    //Hallways
+    vOffset = allocate(filePath, {TL:{x:spaceToFill.TL.x / 3 -hallWidth,y:spaceToFill.TL.y / 3 -hallWidth},BR:{x:spaceToFill.BR.x / 3 +hallWidth,y:spaceToFill.BR.y / 3 +hallWidth},isRoom:false}, 3, 0, emptySpace, filledSpace, vOffset);    
+    vOffset = allocate(filePath, {TL:{x:spaceToFill.TL.x / 3 -hallWidth,y:spaceToFill.TL.y},BR:{x:spaceToFill.TL.x / 3,y:spaceToFill.BR.y},isRoom:false}, 3, 0, emptySpace, filledSpace, vOffset);
+    vOffset = allocate(filePath, {TL:{x:spaceToFill.BR.x / 3,y:spaceToFill.TL.y},BR:{x:spaceToFill.BR.x / 3 +hallWidth,y:spaceToFill.BR.y},isRoom:false}, 3, 0, emptySpace, filledSpace, vOffset);
+    
+    
+    emptySpace.length = 0;
+
+    let innerSpace = {TL:{x:spaceToFill.TL.x / 3,y:spaceToFill.TL.y / 3},BR:{x:spaceToFill.BR.x / 3,y:spaceToFill.BR.y / 3},isRoom:false};
+    
+    //add sides back in
+    emptySpace.push({TL:{x:spaceToFill.TL.x,y:spaceToFill.TL.y},BR:{x:spaceToFill.TL.x / 3 -hallWidth,y:spaceToFill.BR.y},isRoom:false});
+    emptySpace.push({TL:{x:innerSpace.TL.x,y:spaceToFill.TL.y},BR:{x:innerSpace.BR.x,y:spaceToFill.TL.y / 3 -hallWidth},isRoom:false});
+    emptySpace.push({TL:{x:innerSpace.TL.x,y:spaceToFill.BR.y / 3 +hallWidth},BR:{x:innerSpace.BR.x,y:spaceToFill.BR.y},isRoom:false});
+    emptySpace.push({TL:{x:spaceToFill.BR.x / 3 +hallWidth,y:spaceToFill.TL.y},BR:{x:spaceToFill.BR.x,y:spaceToFill.BR.y},isRoom:false});
+    
+    //For each empty space, test the number of hall connections.  Use this to decide how to fill the space.
+
+    for (let i = 0; i < emptySpace.length; i++) {
+        let halls = checkForHalls(filledSpace, emptySpace[i]);
+        let hallCount = halls[0] + halls[1] + halls[2] + halls[3];
+        console.log(hallCount)
+        switch (hallCount) {
+            case 0:
+                vOffset = basicFill(filePath, [emptySpace[i]], filledSpace, vOffset);
+                break;
+            case 1:
+            case 2:
+                vOffset = lineFill(filePath, [emptySpace[i]], filledSpace, vOffset);
+                break;
+            case 3:
+                vOffset = threeHallFill(filePath, [emptySpace[i]], filledSpace, vOffset, halls);
+                break;
+            case 4:
+                vOffset = fourHallFill(filePath, [emptySpace[i]], filledSpace, vOffset);
+                break;
+            default:
+                console.log("Error! Abnormal number of adjacent halls.")
+        }
+    }
+
+    let width = spaceToFill.BR.x - spaceToFill.TL.x;
+    let length = spaceToFill.BR.y - spaceToFill.TL.y;
+    //Generate Outer Wall
+    vOffset = makeWalls(
+        width, length, 4,
+        [[width/3 - hallWidth, width/3, width/3 - hallWidth],
+        [length],[width/3 - hallWidth, width/3, width/3 - hallWidth],[length]],
+        hallWidth, "./" + filePath,
+        (spaceToFill.BR.x + spaceToFill.TL.x) / 2, (spaceToFill.BR.y + spaceToFill.TL.y) / 2, 0, vOffset
+    );
+
+    //Generate Outside Of Building
+    vOffset = flatGenerator(width + 30, length + 30,
+        "./" + filePath, {},
+        (spaceToFill.BR.x + spaceToFill.TL.x) / 2, (spaceToFill.BR.y + spaceToFill.TL.y) / 2, 0, vOffset
+    );
+
+    generateLabels(filledSpace, filePath);
+}
--- src/spacesSharedFunctions.js
+++ src/spacesSharedFunctions.js
@@ -366,9 +366,8 @@ export function generateLabels(filledSpace, filePath) {
         let annotationName = name;
         let position = {x:0,y:0,z:0};
         position.x = (space.TL.x + space.BR.x) / 2;
-        position.y = (space.TL.y + space.BR.y) / 2;
-        //may need to change position.z
-        position.z = 0;
+        position.y = 0;
+        position.z = (space.TL.y + space.BR.y) / 2;
         if (filledSpace.length >= nameCount) {
             fs.appendFileSync(filePath + "Labels.json", `\t{\n\t\t"name": "${name}",\n\t\t"annotationName": "${annotationName}",\n\t\t"position": {\n\t\t\t"x": ${position.x},\n\t\t\t"y": ${position.y},\n\t\t\t"z": ${position.z}\n\t\t}\n\t},\n`)
         } else {
